9217f8462a6982c18a107e82eb5f9f13ca4a2591
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-26 20:04:34 +0800

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dirac_arn" {
-  value       = aws_iam_user.users[0].arn
-  description = "The ARN for user Paul Dirac"
-}
-
-output "all_arns" {
-  value       = aws_iam_user.users[*].arn
-  description = "The ARNs for all users"
-}
